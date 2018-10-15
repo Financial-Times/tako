7886c5a06cc9e4271b1d644b7bb85abaa0349345
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2018-10-15 10:27:15 +0100

    Regenerated secret-squirrel.js -- it definitely did use tabs this time.

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -1,14 +1,18 @@
 module.exports = {
-  files: {
-    allow: ['.env.example', '.npmrc', '.nvmrc'],
-    allowOverrides: []
-  },
-  strings: {
-    deny: [],
-    denyOverrides: [
-      'c29d1180-c8a6-11e8-92c2-f3141da0d6da', // README.md:5
-      'e65c5980-c89e-11e8-90b7-06e060217de1', // README.md:7
-      'adam@braimbridge\\.com' // package.json:5
-    ]
-  }
+	files: {
+		allow: [
+			'.env.example',
+			'.npmrc',
+			'.nvmrc'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'c29d1180-c8a6-11e8-92c2-f3141da0d6da', // README.md:5
+			'e65c5980-c89e-11e8-90b7-06e060217de1', // README.md:7
+			'adam@braimbridge\\.com' // package.json:5
+		]
+	}
 };
